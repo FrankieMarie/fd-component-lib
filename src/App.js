789126869994ff46da1f00294b1cd47977bd6197
FrankieMarie <frankiemarie83@gmail.com>
2018-12-05 09:11:15 -0700

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
-import KubraLoadingSvg from './components/KubraLoadingSvg'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './css/main.css';
+
+// COMPONENTS
+import Buttons from './components/Buttons';
+import CTA from './components/CallToAction';
+import Cards from './components/Cards';
+import Forms from './components/Forms';
+import Images from './components/Images';
+import KubraLoader from './components/KubraLoadingSvg';
+import Navs from './components/Navs';
+import SidebarTopbar from './components/SidebarTopbar';
+import Text from './components/Text';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <KubraLoadingSvg message={'Loading...'} />
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Navs} />
+            <Route exact path="/buttons" component={Buttons} />
+            <Route exact path="/cta" component={CTA} />
+            <Route exact path="/cards" component={Cards} />
+            <Route exact path="/forms" component={Forms} />
+            <Route exact path="/images" component={Images} />
+            <Route exact path="/loader" component={KubraLoader} />
+            <Route exact path="/bars" component={SidebarTopbar} />
+            <Route exact path="/text" component={Text} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
